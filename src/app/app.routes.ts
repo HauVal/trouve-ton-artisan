9abd7d44f436a4f19952c43b4e96fdc72bba8f98
HauVal <valentin.hauspie@laposte.net>
2024-07-25 12:05:26 +0200

    Add nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PersonalDataComponent } from './personal-data/personal-data.component';
 import { AccessibilityComponent } from './accessibility/accessibility.component';
@@ -8,6 +10,7 @@ import { ContactComponent } from './contact/contact.component';
 import { CookieManagementComponent } from './cookie-management/cookie-management.component';
 
 export const routes: Routes = [
+    {path:"", component: HomeComponent},
 
     {path: "legal-notice", component: LegalNoticeComponent},
     {path: "personal-data", component: PersonalDataComponent},
