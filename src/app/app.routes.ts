2c99c2fb89edd01c93600b826159062eb82d23bf
HauVal <valentin.hauspie@laposte.net>
2024-07-25 09:51:23 +0200

    Add link footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
-export const routes: Routes = [];
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
+import { ContactComponent } from './contact/contact.component';
+import { CookieManagementComponent } from './cookie-management/cookie-management.component';
+
+export const routes: Routes = [
+
+    {path: "legal-notice", component: LegalNoticeComponent},
+    {path: "personal-data", component: PersonalDataComponent},
+    {path: "accessibility", component: AccessibilityComponent},
+    {path: "contact", component: ContactComponent},
+    {path: "cookie-management", component: CookieManagementComponent}
+];
+
+export const appRoutingProviders = [
+    provideRouter(routes)
+  ];
